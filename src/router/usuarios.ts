0af4c2fe51b880c379e9b719a3c7dc3d0d40f577
projetosc3con <projetosc3con@gmail.com>
2026-01-20 08:26:10 -0400

    inativar usuarios

--- src/router/usuarios.ts
+++ src/router/usuarios.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { admin } from '../firebase';
 //import { authenticate } from '../middlewares/authenticate';
 import Usuario, { nomeAbreviado } from '../interfaces/Usuario';
+import { authenticate } from '../middlewares/authenticate';
 
 const router = Router();
 
@@ -21,5 +22,20 @@ router.post('/', async (req, res) => {
     res.status(400).json({ error: 'Erro ao criar usuário', details: error });
   }
 }); 
+
+//inativar usuario
+router.post('/inativar/:uid', authenticate, async (req, res) => {
+  const { uid } = req.params;
+  try {
+    await admin.auth().updateUser(uid, { disabled: true });
+    const snapshot = await collection.where('uid', '==', uid).get();
+    for (const doc of snapshot.docs) {
+      await doc.ref.update({ inativo: true });
+    }
+    res.status(200).json({ message: 'Usuário inativado com sucesso' });
+  } catch (error) {
+    res.status(400).json({ error: 'Erro ao inativar usuario', details: error });
+  }
+});
 
 export default router;
