9250a782ce3d364e6f86efde5d05f50b810621f5
projetosc3con <projetosc3con@gmail.com>
2026-01-29 17:23:50 -0400

    checagem diaria ativação

--- api/rotina.ts
+++ api/rotina.ts
@@ -1,7 +1,6 @@
 import Rotinas from "../src/services/rotinas";
 
 export default async function handler(req, res) {
-  //await Rotinas.checarConclusaoInicio();
-  await Rotinas.teste();
+  await Rotinas.checarViagens();
   res.status(200).send("Checagem concluída");
 }
